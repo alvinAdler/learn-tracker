e6f291df9f644a6bc7263631d79f39a59444d2ec
alvinAdler <alvinadlersampow@gmail.com>
2025-07-14 14:23:50 +0800

    Added inputs for markdown

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,52 +1,79 @@
 import React from 'react'
 
-export type InputProps = {
+
+export type BaseInputProps = {
   label: string;
   outerClassname?: string;
   placeholder?: string;
-  currentInputType: "text" | "date"
-} & React.InputHTMLAttributes<HTMLInputElement>
+}
+export type InputProps = BaseInputProps & React.InputHTMLAttributes<HTMLInputElement>
+export type InputDateProps = BaseInputProps & React.InputHTMLAttributes<HTMLInputElement>
+export type InputTextAreaProps = BaseInputProps & React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
-const Input: React.FC<InputProps> = ({ label, outerClassname, placeholder, currentInputType, ...rest }) => {
+type UnionInputs = {
+  TextArea: React.FC<InputTextAreaProps>
+  Date: React.FC<InputDateProps>
+}
 
-  const InputText = () => {
-    return (
+const Input: React.FC<InputProps> & UnionInputs = ({ label, outerClassname, placeholder, ...rest }) => {
+
+  return (
+    <BaseContainer
+      label={label}
+      outerClassname={outerClassname}
+    >
       <input
         className='p-1 border-2 border-solid border-white/50 focus:border-white rounded-md outline-none transition-all' 
         type="text"
         placeholder={placeholder ?? "Enter your text here"}
         {...rest}
       />
-    )
-  }
+    </BaseContainer>
+  )
+}
 
-  const InputDate = () => {
-    return (
+const InputTextArea: React.FC<InputTextAreaProps> = ({ label, outerClassname, ...rest }) => {
+  return (
+    <BaseContainer
+      label={label}
+      outerClassname={outerClassname}
+    >
+      <textarea
+        className='p-1 border-2 border-solid border-white/50 focus:border-white rounded-md outline-none transition-all h-full'
+        {...rest}
+      ></textarea>
+    </BaseContainer>
+  )
+}
+
+const InputDate: React.FC<InputDateProps> = ({ label, outerClassname, placeholder, ...rest }) => {
+  return (
+    <BaseContainer label={label} outerClassname={outerClassname}>
       <input
         className='p-1 border-2 border-solid border-white/50 focus:border-white rounded-md outline-none transition-all dark:[color-scheme:dark]' 
         type="date"
         placeholder={placeholder ?? "Enter your text here"}
         {...rest}
       />
-    )
-  }
+    </BaseContainer>
+  )
+}
 
-  const renderInput = () => {
-    switch(currentInputType){
-      case "date":
-        return <InputDate/>
-      case "text":
-      default:
-        return <InputText/>
-    }
-  }
-
-  return (
+export type BaseContainerType = {
+  outerClassname?: string,
+  children: React.ReactNode,
+  label: string,
+}
+export const BaseContainer: React.FC<BaseContainerType> = ({ outerClassname, label, children }) => {
+  return(
     <div className={`flex flex-col gap-2 ${outerClassname ?? ""}`}>
       <span className='font-bold text-xl'>{label}</span>
-      {renderInput()}
+      {children}
     </div>
   )
 }
+
+Input.TextArea = InputTextArea
+Input.Date = InputDate
 
 export default Input
--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
 import "./styles.css"
+import "../../markdown-styles.css"
 import SETTINGS from "../../settings.json"
 import { capitalizeString } from '../../utils'
-import Input from '../../components/input'
-
+import Input, { BaseContainer } from '../../components/input'
+import ReactMarkdown from 'react-markdown'
 
 const NotesPage = () => {
 
@@ -29,6 +30,8 @@ const NotesPage = () => {
       OPENING_WORDS[2], OPENING_WORDS[3],
     ]
   })())
+  const [note, setNote] = useState("")
+
   const cubeRef = useRef<HTMLDivElement>(null)
   const rotateDeg = useRef<number>(0)
   const displayPointerIndex = useRef<number>(0)
@@ -84,7 +87,7 @@ const NotesPage = () => {
   }
 
   return (
-    <div className='h-full min-h-[100vh] flex flex-col items-center w-full gap-4'>
+    <div className='h-full min-h-[100vh] flex flex-col items-center w-full gap-8'>
       <div className='text-center mt-20'>
         <div className='text-3xl flex flex-col'>
           <div>So, what did you</div>
@@ -99,22 +102,41 @@ const NotesPage = () => {
         </div>
         <p>Write down something new you discovered today</p>
       </div>
-      <div className='flex flex-col self-stretch mt-8 gap-4'>
-        <div className='flex gap-8 items-center'>
+      <form className='flex flex-col self-stretch mt-8 lg:gap-4 gap-8'>
+        <div className='flex gap-4 lg:gap-8 lg:flex-row flex-col items-center'>
           <Input
             label='Title'
-            outerClassname='w-full max-w-[20rem]'
-            currentInputType='text'
+            outerClassname='w-full lg:max-w-[20rem]'
           />
-          <Input
+          <Input.Date
             label='Date'
-            outerClassname='w-full max-w-[20rem]'
-            currentInputType='date'
+            outerClassname='w-full lg:max-w-[20rem]'
           />
         </div>
         <div>
-          
+          <BaseContainer label='Tags'>
+            <p>Pass</p>
+          </BaseContainer>
         </div>
+        <div className='grid gap-4 lg:grid-cols-2'>
+          <Input.TextArea
+            label='Notes Markdown'
+            value={note}
+            onChange={(ev) => setNote(ev.target.value)}
+            outerClassname='h-[30rem]'
+          />
+          <div className='flex flex-col h-[30rem]'>
+            <p className='font-bold text-xl mb-2'>Content</p>
+            <div className='markdown-parent flex-auto p-1 border-2 border-solid border-white/50 focus:border-white rounded-md outline-none transition-all overflow-auto'>
+              <ReactMarkdown>
+                {note}
+              </ReactMarkdown>
+            </div>
+          </div>
+        </div>
+      </form>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ullam voluptate non consequatur natus ipsum vel nihil minima a, amet cupiditate enim laborum ex assumenda eligendi quibusdam dolor obcaecati dolorum voluptatem, similique tempore voluptatum aperiam sed! Minima, neque mollitia. Et mollitia sint quaerat in aperiam inventore corrupti, expedita dignissimos beatae.</p>
       </div>
 
     </div>
